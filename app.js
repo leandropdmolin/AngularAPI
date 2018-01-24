681cae0fdb91c0b69aef6d821e9820035de2f933
Leandro <leandro.swk@hotmail.com>
2018-01-24 22:58:25 +0000

    Angular Test 1

---
+++ app.js
@@ -0,0 +1,20 @@
+var app = angular.module('AngularAPI',['ngRoute']);
+
+app.controller('ListController', ['$scope', '$http', function($scope, $http){
+  $scope.results = [];
+  $scope.search = function(){
+      $http({
+        methods: 'GET',
+        url: 'https://api.flickr.com/services/rest',
+        params: {
+          method: 'flickr.photos.search',
+          api_key: 'b83663df0f2553ffc7d00878d481de15',
+          format: 'json',
+          text: $scope.searchTerm,
+          nojsoncallback: 1
+        }
+      }).then(function(data){
+        $scope.results = data;
+      });
+  };
+}]);
